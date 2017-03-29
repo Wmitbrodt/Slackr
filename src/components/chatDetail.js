f9fcc5838ae40bdeeec773dc83ce3b08b82bbd23
Will <wmitbrodt@gmail.com>
2017-03-28 19:43:23 -0700

    created reducers for new room and join rooms

--- src/components/chatDetail.js
+++ src/components/chatDetail.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import { Alert } from 'react-bootstrap'
+import { Alert, Panel } from 'react-bootstrap'
 
 export default (props) => {
   return (
     <div>
-      <Alert bsStyle="warning">
-        <strong>Will</strong> : some random message goes here..
-      </Alert>
+      <Panel bsStyle="info" header={props.user}>
+        {props.message}
+      </Panel>
     </div>
   )
 }
--- src/reducers/initialState.js
+++ src/reducers/initialState.js
@@ -9,5 +9,22 @@ export default {
     { user: 'Michael Jackson',
       message: 'Tee-heee-heee!'
     }
-  ]
+  ],
+
+  rooms: [
+    { title: 'events',
+     messages: [ { user: 'will', message: 'hello world'}, { user:'Bill Murray', mesage: 'Dogs and Cats living together...mass hysteria'} ]
+     },
+    { title: 'general',
+     messages: [ {user: 'Michael Jackson', message: 'Tee-hee-hee'}, {user: 'Bill Murray', message: 'Michael, you were always super talented...'}]
+    }
+    { title: 'january-2017-bootcamp',
+     messages: [ {user: 'will', message: 'S.O.M. Smear'}, {user: 'Bill Murray', message: 'Gotta love a good Smear...!'}]
+    }
+  ],
+
+  activeRoom: {
+    title: '',
+    messages: []
+  }
 }
