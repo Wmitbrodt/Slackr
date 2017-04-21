8989e4b04061595849a682a770e0fa6ccd7cab2e
Brian Ting <briantingtc@gmail.com>
2017-04-20 19:56:27 -0700

    created webpack production build

---
+++ webpack.config.production.js
@@ -0,0 +1,26 @@
+var webpack = require('webpack')
+var path = require('path')
+
+module.exports = {
+  noInfo: false,
+  entry: [
+    'eventsource-polyfill',
+    './src/index'
+  ],
+  target: 'web',
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {test: /\.js$/, include: path.join(__dirname, 'src'), loaders: ['babel']},
+      {test: /(\.css)$/, loaders: ['style', 'css']},
+      {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file'},
+      {test: /\.(woff|woff2)$/, loader: 'url?prefix=font/&limit=5000'},
+      {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream'},
+      {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml'}
+    ]
+  }
+};
